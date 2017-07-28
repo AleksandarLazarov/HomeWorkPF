a1776198994dd0104016facf8a910b15e8670cf7
AleksandarLazarov <aplazarov@abv.bg>
2017-07-28 18:22:53 +0300

    Arrays

--- Array and List Algorithms - Exercises/1. Shoot List Elements/Shoot List Elements.cs
+++ Array and List Algorithms - Exercises/1. Shoot List Elements/Shoot List Elements.cs
@@ -10,7 +10,68 @@ namespace _1.Shoot_List_Elements
     {
         static void Main(string[] args)
         {
+            int numbreCount = 0;
+            int strCount = 0;
+            var num = new List<int>();
+            var input = Console.ReadLine();
+            int lastRemoved = 0;
+            while (input != "stop")
+            { 
+                if (input == "bang")
+                {
+                    strCount++;
+                    double avr = num.Count > 0 ? num.Average() : 0;
 
+                    if (num.Count == 0 && strCount>numbreCount)
+                    {
+                        Console.WriteLine($"nobody left to shoot! last one was {lastRemoved}");
+                        return;
+                    }
+                    for (int i = 0; i < num.Count; i++)
+                    {                                                
+                        if (num.Count > 0 && avr > num[i])
+                        {
+                            lastRemoved = num[i];
+                            Console.WriteLine($"shot {num[i]}");
+                            num.RemoveAt(i);
+                            
+                            for (int ii = 0; ii < num.Count; ii++)
+                            {
+                                num[ii]--;
+                            }
+                            if (num.Count == 1)
+                            {
+                                lastRemoved = num[0];
+                                Console.WriteLine($"shot {num[0]}");
+                                num.RemoveAt(0);
+                            }
+                            break;
+                        }
+                        if (num.Count==0)
+                        {
+                            break;
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    num.Insert(0,int.Parse(input));
+                    numbreCount++;
+                }
+
+                input = Console.ReadLine();
+
+            }
+            if (num.Count==0)
+            {
+                Console.WriteLine($"you shot them all. last one was {lastRemoved}");
+            }
+            if (input=="stop" && num.Count>0)
+            {
+                Console.WriteLine("survivors: " + string.Join(" ", num));
+            }
+            
 
         }
     }
